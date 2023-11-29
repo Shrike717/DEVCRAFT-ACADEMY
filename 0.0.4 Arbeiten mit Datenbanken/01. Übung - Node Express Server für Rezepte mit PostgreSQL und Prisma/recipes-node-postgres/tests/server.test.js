68b0e8ea0cb057c37e5fd1b7dbdead70d480887e
Daniel Bauer <dan717@gmx.de>
2023-11-29 15:56:12 +0100

    Added all tests for POST user

--- 0.0.4 Arbeiten mit Datenbanken/01. Übung - Node Express Server für Rezepte mit PostgreSQL und Prisma/recipes-node-postgres/tests/server.test.js
+++ 0.0.4 Arbeiten mit Datenbanken/01. Übung - Node Express Server für Rezepte mit PostgreSQL und Prisma/recipes-node-postgres/tests/server.test.js
@@ -28,25 +28,77 @@ describe('Express Server Tests', () => {
 		jest.resetAllMocks();
 	});
 
-	// Wir beginnen eine Testgruppe für unsere Benutzer-Endpunkte.
 	// ******** User Tests ********
-	test('should create a new user', async () => {
-		// Wir senden eine POST-Anfrage an den '/users' Endpunkt, um einen neuen Benutzer zu erstellen.
-		const res = await request(app).post('/users').send({
-			username: 'Testuser',
-			email: 'testuser@example.com',
+
+	// POST /users Endpoint Tests:
+
+	describe('POST /users Endpoint Tests', () => {
+		// Anlegen eines neuen Benutzers:
+		test('sollte einen neuen Benutzer anlegen', async () => {
+			// Wir senden eine POST-Anfrage an den '/users' Endpunkt, um einen neuen Benutzer zu erstellen.
+			const res = await request(app).post('/users').send({
+				username: 'Testuser',
+				email: 'testuser@example.com',
+			});
+
+			// Wenn der Statuscode 409 ist, erwarten wir, dass die Fehlermeldung korrekt ist.
+			if (res.statusCode === 409) {
+				expect(res.body.message).toEqual(
+					'Es existiert bereits ein Benutzer mit dieser E-Mail oder diesem Benutzernamen'
+				);
+			} else {
+				// Wenn der Statuscode 201 ist, erwarten wir, dass der Benutzername und die E-Mail des neuen Benutzers korrekt sind.
+				expect(res.statusCode).toEqual(201);
+				expect(res.body.newUser.username).toEqual('Testuser');
+				expect(res.body.newUser.email).toEqual('testuser@example.com');
+			}
 		});
 
-		// Wenn der Statuscode 409 ist, erwarten wir, dass die Fehlermeldung korrekt ist.
-		if (res.statusCode === 409) {
+		// Anlegen eines neuen Benutzers mit fehlenden Feldern:
+		test('sollte einen Fehler zurückgeben, wenn Felder fehlen', async () => {
+			// Wir senden eine POST-Anfrage an den '/users' Endpunkt, um einen neuen Benutzer zu erstellen.
+			const res = await request(app).post('/users').send({
+				username: 'Testuser',
+			});
+
+			// Wir erwarten, dass der Statuscode 400 ist und die Fehlermeldung korrekt ist.
+			expect(res.statusCode).toEqual(400);
+			expect(res.body.message).toEqual('Bitte alle Felder ausfüllen');
+		});
+
+		// Anlegen eines neuen Benutzers mit bereits existierendem Benutzernamen:
+		test('sollte einen Fehler zurückgeben, wenn der Benutzername bereits existiert', async () => {
+			// Wir senden eine POST-Anfrage an den '/users' Endpunkt, um einen neuen Benutzer zu erstellen.
+			const res = await request(app).post('/users').send({
+				username: 'Testuser',
+				email: 'testuser@example.com',
+			});
+
+			// Wir erwarten, dass der Statuscode 409 ist und die Fehlermeldung korrekt ist.
+			expect(res.statusCode).toEqual(409);
 			expect(res.body.message).toEqual(
 				'Es existiert bereits ein Benutzer mit dieser E-Mail oder diesem Benutzernamen'
 			);
-		} else {
-			// Wenn der Statuscode 201 ist, erwarten wir, dass der Benutzername und die E-Mail des neuen Benutzers korrekt sind.
-			expect(res.statusCode).toEqual(201);
-			expect(res.body.newUser.username).toEqual('Testuser');
-			expect(res.body.newUser.email).toEqual('testuser@example.com');
-		}
+		});
+	});
+
+	// GET /users Endpoint Tests:
+
+	describe('GET /users Endpoint Tests', () => {
+		// Abrufen aller Benutzer:
+		test('sollte alle Benutzer zurückgeben', async () => {
+			// Wir senden eine GET-Anfrage an den '/users' Endpunkt, um alle Benutzer abzurufen.
+			const res = await request(app).get('/users');
+
+			// Wir erwarten, dass der Statuscode 200 ist und die Benutzer korrekt sind.
+			expect(res.statusCode).toEqual(200);
+			expect(res.body).toEqual([
+				{
+					id: 1,
+					username: 'Testuser',
+					email: 'testuser@example.com',
+				},
+			]);
+		});
 	});
 });
