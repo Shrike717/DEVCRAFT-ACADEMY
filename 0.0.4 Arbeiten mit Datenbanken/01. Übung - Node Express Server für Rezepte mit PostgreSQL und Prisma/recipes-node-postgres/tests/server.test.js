d80f1c240dbc44c40acf0eac75bc4f4394eca178
Daniel Bauer <dan717@gmx.de>
2023-11-29 13:09:15 +0100

    Changed test logic to mock the prisma model User completely

--- 0.0.4 Arbeiten mit Datenbanken/01. Übung - Node Express Server für Rezepte mit PostgreSQL und Prisma/recipes-node-postgres/tests/server.test.js
+++ 0.0.4 Arbeiten mit Datenbanken/01. Übung - Node Express Server für Rezepte mit PostgreSQL und Prisma/recipes-node-postgres/tests/server.test.js
@@ -1,123 +1,52 @@
+// Wir importieren den PrismaClient aus dem Prisma-Client-Paket.
 const { PrismaClient } = require('@prisma/client');
-const { execSync } = require('child_process');
+// Wir importieren das supertest-Paket, um HTTP-Anfragen an unseren Server zu senden.
 const request = require('supertest');
+// Wir importieren die app und start Funktionen aus unserer Hauptserverdatei.
 const { app, start } = require('../index.js');
 
+// Wir simulieren das '@prisma/client' Paket, um die Datenbankoperationen in unseren Tests zu kontrollieren.
+jest.mock('@prisma/client');
+
+// Wir beginnen eine Testgruppe für unseren Express-Server.
 describe('Express Server Tests', () => {
-	let prisma;
-	let server;
-	const port = process.env.NODE_ENV === 'test' ? 3001 : 3000;
+	let server; // Wir definieren eine Variable für unseren Server.
+	const port = 3001; // Wir definieren den Port, auf dem unser Server läuft.
 
-	beforeAll(async () => {
-		// Use the main database
-		process.env.DATABASE_URL = process.env.DATABASE_URL;
-
-		// Run Prisma migrations on the main database and log the result
-		const result = execSync('npx prisma migrate dev --preview-feature', {
-			encoding: 'utf8',
-		});
-		console.log(result);
-
-		prisma = new PrismaClient();
+	// Bevor alle Tests ausgeführt werden, starten wir unseren Server auf dem definierten Port.
+	beforeAll(() => {
 		server = start(port);
 	});
 
+	// Nachdem alle Tests ausgeführt wurden, schließen wir unseren Server.
 	afterAll((done) => {
-		// Disconnect the Prisma client
-		prisma.$disconnect();
-
-		// Close the server and pass the done callback
 		server.close(done);
 	});
 
+	// Vor jedem Test setzen wir alle simulierten Funktionen zurück.
+	beforeEach(() => {
+		jest.resetAllMocks();
+	});
+
+	// Wir beginnen eine Testgruppe für unsere Benutzer-Endpunkte.
 	// ******** User Tests ********
-	describe('User Tests', () => {
-		// Teste POST /users mit korrekten Feldern
-		describe('POST /users', () => {
-			it('should create a new user', async () => {
-				const res = await request(app).post('/users').send({
-					username: 'Testuser',
-					email: 'testuser@example.com',
-				});
-				expect(res.statusCode).toEqual(201);
-				expect(res.body.newUser.username).toEqual('Testuser');
-				expect(res.body.newUser.email).toEqual('testuser@example.com');
-			});
+	test('should create a new user', async () => {
+		// Wir senden eine POST-Anfrage an den '/users' Endpunkt, um einen neuen Benutzer zu erstellen.
+		const res = await request(app).post('/users').send({
+			username: 'Testuser',
+			email: 'testuser@example.com',
 		});
 
-		// Teste POST /users mit fehlenden Feldern
-		describe('POST /users', () => {
-			it('should return error message', async () => {
-				const res = await request(app).post('/users').send({
-					username: 'Testuser',
-				});
-				expect(res.statusCode).toEqual(400);
-				expect(res.body.message).toEqual('Bitte alle Felder ausfüllen');
-			});
-		});
-
-		// Teste GET /users
-		describe('GET /users', () => {
-			it('should return all users', async () => {
-				const res = await request(app).get('/users');
-				expect(res.statusCode).toEqual(200);
-				expect(res.body.length).toBeGreaterThanOrEqual(1);
-			});
-		});
-
-		// Teste GET /users/:userId mit korrekter ID
-		describe('GET /users/:userId', () => {
-			it('should return a single user', async () => {
-				const res = await request(app).get('/users/1');
-				expect(res.statusCode).toEqual(200);
-				expect(res.body.id).toEqual(1);
-			});
-		});
-
-		// Teste GET /users/:userId mit falscher ID
-		describe('GET /users/:userId', () => {
-			it('should return error message', async () => {
-				const res = await request(app).get('/users/100');
-				expect(res.statusCode).toEqual(404);
-				expect(res.body.message).toEqual('Benutzer nicht gefunden');
-			});
-		});
-
-		// Teste PUT /users/:userId
-		describe('PUT /users/:userId', () => {
-			it('should update a user', async () => {
-				const res = await request(app).put('/users/1').send({
-					username: 'Testuser Updated!',
-					email: 'testuser@example.com Updated!',
-				});
-				expect(res.statusCode).toEqual(200);
-				expect(res.body.updatedUser.username).toEqual(
-					'Testuser Updated!'
-				);
-				expect(res.body.updatedUser.email).toEqual(
-					'testuser@example.com Updated!'
-				);
-			});
-		});
-
-		// Teste DELETE /users/:userId mit korrekter ID
-		describe('DELETE /users/:userId', () => {
-			it('should delete a user', async () => {
-				const res = await request(app).delete('/users/1');
-				expect(res.statusCode).toEqual(200);
-				expect(res.body.message).toEqual(
-					'Benutzer erfolgreich gelöscht'
-				);
-			});
-		});
-
-		// Teste DELETE /users/:userId mit falscher ID
-		describe('DELETE /users/:userId', () => {
-			it('should return error message', async () => {
-				const res = await request(app).delete('/users/100');
-				expect(res.statusCode).toEqual(404);
-				expect(res.body.message).toEqual('Benutzer nicht gefunden');
-			});
-		});
+		// Wenn der Statuscode 409 ist, erwarten wir, dass die Fehlermeldung korrekt ist.
+		if (res.statusCode === 409) {
+			expect(res.body.message).toEqual(
+				'Es existiert bereits ein Benutzer mit dieser E-Mail oder diesem Benutzernamen'
+			);
+		} else {
+			// Wenn der Statuscode 201 ist, erwarten wir, dass der Benutzername und die E-Mail des neuen Benutzers korrekt sind.
+			expect(res.statusCode).toEqual(201);
+			expect(res.body.newUser.username).toEqual('Testuser');
+			expect(res.body.newUser.email).toEqual('testuser@example.com');
+		}
 	});
 });
